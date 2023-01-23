c70df936e60fada4d213d34bfa9992f4d4e357d2
Goves7 <collingoves1996@gmail.com>
2023-01-23 14:44:25 +0530

    New Commit ToUnderstandGitPush.java

---
+++ wcsa4seleniumproject/src/gitDemo/ToUnderstandGitPush.java
@@ -0,0 +1,11 @@
+package gitDemo;
+
+import org.testng.annotations.Test;
+
+public class ToUnderstandGitPush {
+
+	@Test
+	public void gitPushDemo() {
+		System.out.println("New Push to Git Hub");
+	}
+}
